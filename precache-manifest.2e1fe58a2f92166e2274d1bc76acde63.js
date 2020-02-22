bd8a1cca1203cff9bb5c4f052ce60f5d551f3c78
JOOIL CHANG <heikifm@jichangMac.local>
2020-02-22 15:55:38 +0900

    Updates

---
+++ precache-manifest.2e1fe58a2f92166e2274d1bc76acde63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c05fd142b2192e2a5499c0798b0e41",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "3054e639dce21ee0803d",
+    "url": "/movie_app_2019/static/css/main.74b40302.chunk.css"
+  },
+  {
+    "revision": "b79560b6446b38653919",
+    "url": "/movie_app_2019/static/js/2.2d5b8db8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2019/static/js/2.2d5b8db8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3054e639dce21ee0803d",
+    "url": "/movie_app_2019/static/js/main.10ee82e5.chunk.js"
+  },
+  {
+    "revision": "13d83943eef6cf8f8c6f",
+    "url": "/movie_app_2019/static/js/runtime-main.57c632df.js"
+  }
+]);
